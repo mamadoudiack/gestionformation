40003d6137b45cb96acc8628d7cfdc0e7ae15a24
unknown <diackmo.dm@gmail.com>
2018-09-06 13:06:50 +0000

    3er commit du gestion form

--- src/main/java/sn/objis/gestionformation/App.java
+++ src/main/java/sn/objis/gestionformation/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         
-        system.out.println( "Hello World!" ); 
+     
     }
 }
---
+++ src/main/java/sn/objis/gestionformation/Test.java
@@ -0,0 +1,8 @@
+package sn.objis.gestionformation;
+
+public class Test {
+	public void main()
+	{
+		system.out.println( "Hello World!" );
+	}
+}
