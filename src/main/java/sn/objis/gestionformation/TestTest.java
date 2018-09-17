1e688a1d5c9d6b148f1c1ea9f663ed3907865da8
unknown <diackmo.dm@gmail.com>
2018-09-17 18:36:23 +0000

    test sonar

---
+++ src/main/java/sn/objis/gestionformation/TestTest.java
@@ -0,0 +1,11 @@
+package sn.objis.gestionformation;
+
+import junit.framework.TestCase;
+
+public class TestTest extends TestCase {
+
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/main/java/sn/objis/gestionformation/testsonar.java
@@ -0,0 +1,15 @@
+package sn.objis.gestionformation;
+
+/**
+ * Hello world!
+ *
+ */
+public class testsonar 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "tst sonar" );
+        
+     
+    }
+}
